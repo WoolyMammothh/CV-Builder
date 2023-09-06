d04bf92e6d5150b019a559b544a21af17fc3955c
WoolyMammothh <davidojobo1@gmail.com>
2023-09-06 15:54:22 +0100

    Fixed up syntax for readability

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const displayEls = () => {
-  let name = document.getElementsByClassName("field-item")[0].value;
-  let displayName = document.getElementById("name");
-  displayName.innerHTML = name;
-}
-
 document.addEventListener("DOMContentLoaded",  () => {
-  const btn = document.getElementById("btn");
-  btn.addEventListener("click", displayEls);
+  let displayName = document.getElementById("name");
+  let details = document.getElementById("details");
+
+  const displayEls = () => {
+    let name = document.getElementsByClassName("field-item")[0].value;
+    let city = document.getElementById("city").value ;
+    let country = document.getElementById("country").value;
+    let phone = document.getElementById("phone").value;
+    let website = document.getElementById("website").value;
+    displayName.innerHTML = name;
+    details.innerHTML = `${city}, ${country} | ${phone} | <a href="${website}">${website}</a>`; 
+  }
+
+
+    const btn = document.getElementById("btn");
+    btn.addEventListener("click", displayEls);
 });
